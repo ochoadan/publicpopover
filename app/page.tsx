70f0588cc52534b23728518983a1dc56bd9f4980
Dan <42155753+ochoadan@users.noreply.github.com>
2023-11-01 12:52:24 -0400

    Initial

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+const Page = () => {
+  return (
+    <div className="mx-auto max-w-7xl px-6 lg:px-8">
+      <div className="pt-16 flow-root sm:pt-24">
+        <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+          <img
+            src="/img/2.png"
+            alt="App screenshot"
+            width={2432}
+            height={1442}
+            className="rounded-md shadow-2xl ring-1 ring-gray-900/10"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
